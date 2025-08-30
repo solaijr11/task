7fd5d575b55ece690d187a5612cdd25233f50378
solaijr11 <solaisiva415@gmail.com>
2025-08-30 19:23:43 +0530

    folder2 added

---
+++ folder2/basic.py
@@ -0,0 +1,53 @@
+#print statement
+print("hello world")
+print("python")
+
+#end argument in print
+print("hello world",end="  ")   # default new line "\n"
+print("hi")
+
+#sep argument in print 
+print("hi","how","are","you")    # default seperate , give space 
+print("hi","how","are","you",sep="=>")
+
+#math in print
+print(2+2)
+
+#multiline command
+'''
+command
+command
+
+'''
+
+# escape sequence 
+'''
+\n = new line
+\t = tab space
+'''
+# print("'it\'s Hero time!'\n\t - Ben10") quiz
+
+
+#data type
+'''
+simple => integer , float , string , boolean
+complex => list , tuple , dictionary 
+
+'''
+
+#assign 
+# card
+name = "siva"
+age=24
+available=True
+rating=4.5
+
+print(type(age))    #type infered (self identify)
+
+#multiple assign
+group,row,value="devops",2,9.5
+
+print(type(value))
+print(group[0]) # indexing
+
+#changes for pull
